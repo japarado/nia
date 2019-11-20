89ca233118a2fb66fc94bcac384d0156167617c2
Justin Parado <justin.parado@dice205.com>
2019-11-20 19:09:46 +0800

    Removed executable method (main()) from NaturalLog.java

--- src/rootcomputer/expression/NaturalLog.java
+++ src/rootcomputer/expression/NaturalLog.java
@@ -5,9 +5,6 @@ import java.math.RoundingMode;
 
 public class NaturalLog extends Expression
 {
-    public static void main(String[] args)
-    {
-    }
     public NaturalLog(int decimalPrecision)
     {
         super(decimalPrecision);
@@ -16,13 +13,13 @@ public class NaturalLog extends Expression
     @Override
     public BigDecimal computeFunction(BigDecimal value)
     {
-        return new BigDecimal( Math.log( value.add(new BigDecimal(1)).doubleValue() ) ).add(new BigDecimal(1)).setScale(this.getDecimalPrecision(), RoundingMode.HALF_UP);
+        return new BigDecimal(Math.log(value.add(new BigDecimal(1)).doubleValue())).add(new BigDecimal(1)).setScale(this.getDecimalPrecision(), RoundingMode.HALF_UP);
     }
 
     @Override
     public BigDecimal computePrime(BigDecimal value)
     {
-        BigDecimal numerator = new BigDecimal( Math.log( value.doubleValue() + 1 ) ).add(new BigDecimal(1));
+        BigDecimal numerator = new BigDecimal(Math.log(value.doubleValue() + 1)).add(new BigDecimal(1));
         BigDecimal denominator = new BigDecimal(1).divide((value.add(new BigDecimal(1))), this.getDecimalPrecision(), RoundingMode.HALF_UP);
 
         return value.subtract(numerator.divide(denominator, RoundingMode.HALF_UP));
